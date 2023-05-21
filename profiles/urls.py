e80159c50d586c9e0780dd8e8024f6c6f5de53c3
Nemi Desai <desainemi123@gmail.com>
2023-05-21 09:52:43 -0700

    personal user profile page initial setup (link to personal profile page made)

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "profiles"
 urlpatterns = [
     path("<int:user_id>/", views.profile, name="profile"),
+    path("<int:user_id>/page", views.user_profile, name="user-profile"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -13,3 +13,14 @@ def profile(request, user_id):
         }
         return render(request, 'profiles/profile.html', context)
     return None
+
+def user_profile(request, user_id):
+    profile_user = User.objects.get(pk=user_id)
+    if profile_user is not None:
+        reviews = Review.objects.filter(user=profile_user)
+        context =  {
+            'profile_user': profile_user,
+            'reviews': reviews
+        }
+        return render(request, 'profiles/user-profile.html', context)
+    return None
