9771c5fef8da43078ea61e14cb39015583f40511
Veena Kommu <vkommu@g.ucla.edu>
2023-05-24 19:47:26 -0700

    Users can now like and dislike reviews left by others

--- reviews/models.py
+++ reviews/models.py
@@ -10,4 +10,8 @@ class Review(models.Model):
 
     rating = models.IntegerField(blank=False, default=0)
     text = models.CharField(blank=False,max_length=500)
-    images = models.ImageField(upload_to='review_images/',blank=True);
+    images = models.ImageField(upload_to='review_images/',blank=True)
+
+    likes=models.ManyToManyField(User, related_name='liked_reviews', blank=True)
+    dislikes=models.ManyToManyField(User, related_name='disliked_reviews', blank=True)
+    
