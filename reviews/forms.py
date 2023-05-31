e16d2989035b68498cf426d02a8274d3ca9d9d90
Krisha <krishachokshi@s-164-67-207-200.resnet.ucla.edu>
2023-05-31 04:19:25 -0700

    Added in the css for the add a review page

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class ReviewForm(forms.Form):
-    rating_choices = [(i, str(i)) for i in range(1,6)]  # Create a list of choices from 0 to 5
-    rating = forms.ChoiceField(choices=rating_choices)
+    rating_choices = [(i, str(i)) for i in range(1, 6)]  # Create a list of choices from 1 to 5
+    rating = forms.ChoiceField(choices=rating_choices, widget=forms.Select(attrs={'class': 'custom-select'}))
 
-    text = forms.CharField(max_length=500);
-    images = forms.ImageField(required=False);
+    text = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class': 'custom-textarea'}))
+    images = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'class': 'custom-file-input'}))
