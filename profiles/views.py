0a3ca0097a8033cea98ef08cc7ceaa2b55d6299f
Nemi Desai <desainemi123@gmail.com>
2023-06-02 11:24:17 -0700

    image successfully stored in backend

--- profiles/views.py
+++ profiles/views.py
@@ -40,10 +40,11 @@ class ProfilePictureForm(forms.Form):
     profile_picture = forms.ImageField()
 
 def upload_profile_picture(request, user_id):
+    user = User.objects.get(pk=user_id)
     if request.method == 'POST':
         form = ProfilePictureForm(request.POST, request.FILES)
         if form.is_valid():
-            profile_user = UserProfile.objects.get(pk=user_id)
+            profile_user = UserProfile.objects.get(user=user)
             profile_user.profile_picture = form.cleaned_data['profile_picture']
             profile_user.save()
             return HttpResponseRedirect(reverse("profiles:user-profile", args=(user_id,)))  # Redirect to the user's profile page
